30d52cbfdf259eaba80c081f81c01095eb4898c1
antonmorya <antonmorya@gmail.com>
2016-04-29 16:58:59 +0300

    0.3.1 typing

--- script.js
+++ script.js
@@ -2,29 +2,24 @@
 
 //function startTheDance() {}
 
-function gimmeLabel(class1, class2, string) {
-	var label = document.createElement("label");
+function gimmeSpan(class1, class2, string) {
+	var newspan = document.createElement("span");
 	var t = document.createTextNode('yes');
-	label.appendChild(t);
-	label.setAttribute('class', class1 + ' ' + class2);	
-	main.appendChild(label);
+	newspan.appendChild(t);
+	newspan.setAttribute('class', class1 + ' ' + class2);	
+	main.appendChild(newspan);
 
-	var letter;
-	for (var i = 0; i < string.length; i++) {
-		letter = string[i];
-		setTimeout(function() {label.innerHTML += letter}, 500); //в этом месте задержки нет :(
-	}
+	$(function () {
+		$(newspan).typed({
+			strings: [string],
+			typeSpeed: 20,
+			startDelay: 1000
+		});
+	});
 }
 
 function gimmeBr() {
 	
 }
 
-function print(string) {
-	for(var i = 0; i<string.length; i++) {
-		
-	}
-}
-
-
-gimmeLabel('c2', 'l4', ' you did it!');
+gimmeSpan('c2', 'l4', ' you did it!');
